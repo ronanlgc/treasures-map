122500f03f3ee1b180a640b8e1ef5a39710bfd59
LE GOUIC Ronan <ronan.legouic@supinfo.com>
2022-04-19 20:42:25 +0200

    fix

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-treasures-map
+# Carte aux trésors
+
+Ce repository est une solution à l'exercice "La carte aux trésors" de CarbonIT
+
+## Utilisation
+
+- Cloner le projet
+- Utiliser la commande ```mvn install```
+- 
--- src/main/java/com/carbon/treasuresmap/InputReader.java
+++ src/main/java/com/carbon/treasuresmap/InputReader.java
@@ -1,7 +1,6 @@
 package com.carbon.treasuresmap;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,6 +10,9 @@ public class InputReader {
     public static List<String> read(String path) throws FileNotFoundException {
 
         List<String> lines = new ArrayList<>();
+
+//        InputStream in = InputReader.class.getResourceAsStream(path);
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
         File inputFile = new File(path);
         Scanner sc = new Scanner(inputFile);
 
